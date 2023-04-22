2374ba604a8949ec3989cd85eebe178cd2bb390b
hamchick <chickenhamgod@gmail.com>
2023-04-22 18:32:09 +0800

    Initial commit

---
+++ Data/MVCdemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMVCCRUD.Data
+{
+    public class MVCdemoDbContext : DbContext
+    {
+        public MVCdemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
