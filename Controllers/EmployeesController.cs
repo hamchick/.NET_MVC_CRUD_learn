79b145fbf769be0fb99ef9f9bcc22efc59e2be52
hamchick <chickenhamgod@gmail.com>
2023-04-22 20:50:10 +0800

    do Add and View in app with EntityFrameworkCore

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,59 @@
+﻿using ASPNETMVCCRUD.Data;
+using ASPNETMVCCRUD.Models;
+using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMVCCRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly MVCdemoDbContext mvcdemoDbContext;
+
+        public EmployeesController(MVCdemoDbContext mvcdemoDbContext)
+        {
+            this.mvcdemoDbContext = mvcdemoDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employees = await mvcdemoDbContext.Employees.ToListAsync();
+            return View(employees);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+           
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth
+            };
+
+            await mvcdemoDbContext.Employees.AddAsync(employee);
+            await mvcdemoDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult View(Guid id)
+        {
+            var employee = mvcdemoDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            return View(employee);
+        }
+
+    }
+}
